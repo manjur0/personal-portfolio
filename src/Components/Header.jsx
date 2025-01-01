5a2b43ee8895fae4dc7a88812467455937f79264
manjur0 <sfmanjur21@gmail.com>
2025-01-01 19:49:13 +0600

    navbar completed with responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,6 +3,8 @@
  * License: Apache-2.0
  * Email: sfmanjur21@gmail.com
  */
+import { FiMenu } from "react-icons/fi";
+import { FaXmark } from "react-icons/fa6";
 
 import { useState } from "react";
 import logo from "../assets/images/logo.svg";
@@ -12,7 +14,7 @@ const Header = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center  md:px-6 md:grid md:grid-cols-[1fr, 3fr, 1fr]">
+      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center  md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
         <h1>
           <a href="/" className="logo">
             <img
@@ -28,7 +30,9 @@ const Header = () => {
             onClick={() => setNavOpen((prevent) => !prevent)}
             className="menu-btn md:hidden"
           >
-            <span className="material-symbols-rounded">menu</span>
+            <span className="material-symbols-rounded">
+              {navOpen ? <FaXmark /> : <FiMenu />}
+            </span>
           </button>
           <Navbar navOpen={navOpen} />
         </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,13 +5,33 @@
  */
 
 // Node modules
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 // main Navbar component
 const Navbar = ({ navOpen }) => {
-  const lastActiveLink = useRef(null);
-  const activebox = useRef(null);
+  const lastActiveLink = useRef();
+  const activeBox = useRef();
+
+  // initActiveBox function to set the active box position and size based on the last active link
+  const initActiveBox = () => {
+    activeBox.current.style.top = lastActiveLink.current.offsetTop + "px";
+    activeBox.current.style.left = lastActiveLink.current.offsetLeft + "px";
+    activeBox.current.style.width = lastActiveLink.current.offsetWidth + "px";
+    activeBox.current.style.height = lastActiveLink.current.offsetHeight + "px";
+  };
+  // Use effect
+  useEffect(initActiveBox, []);
+  window.addEventListener("resize", initActiveBox);
+
+  // active current link function
+  const activeCurrentLink = (e) => {
+    lastActiveLink.current?.classList.remove("active");
+    e.target.classList.add("active");
+    lastActiveLink.current = e.target;
+
+    initActiveBox();
+  };
 
   // navItems
   const navItems = [
@@ -46,17 +66,25 @@ const Navbar = ({ navOpen }) => {
   return (
     <nav className={`navbar ${navOpen ? "active" : ""}`}>
       {navItems.map(({ label, link, className, ref }, key) => (
-        <a href={link} className={className} key={key} ref={ref}>
+        <a
+          onClick={activeCurrentLink}
+          href={link}
+          className={className}
+          key={key}
+          ref={ref}
+        >
           {label}
         </a>
       ))}
-      <div className="active-box" ref={activebox}></div>
+      <div className="active-box" ref={activeBox}></div>
     </nav>
   );
 };
 
+
+// prop types validation
 Navbar.propTypes = {
   navOpen: PropTypes.bool.isRequired,
-};
+}; 
 
 export default Navbar;
