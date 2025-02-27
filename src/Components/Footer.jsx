754832a3eb98fc128ddd0f0178a73e4faad3c839
manjur0 <sfmanjur21@gmail.com>
2025-02-27 22:24:48 +0600

    footer completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -54,28 +54,71 @@ const Footer = () => {
   return (
     <footer className="section">
       <div className="container">
-        <div className="">
-          <h2 className="headline-1">Let&apos;s work together today!</h2>
-          <ButtonPrimary
-            classes={"btn btn-third"}
-            href="mailto:mdpeeku@gmail.com"
-            label="Let's talk"
-            icon={<MdKeyboardArrowRight />}
-          />
-        </div>
-        <div className="">
-          <div>
-            <p className="">Sitemap</p>
-            <ul>
-              {sitemap.map(({ label, href }, key) => (
-                <li key={key}>
-                  <a href={href} className="">
-                    {label}
-                  </a>
-                </li>
-              ))}
-            </ul>
+        <div className=" lg:grid lg:grid-cols-2 ">
+          <div className="mb-10">
+            <h2 className="headline-1 mb-8 lg:max-w-[12ch]">
+              Let&apos;s work together today!
+            </h2>
+            <ButtonPrimary
+              classes={"btn btn-third"}
+              href="mailto:mdpeeku@gmail.com"
+              label="Let's talk"
+              icon={<MdKeyboardArrowRight />}
+            />
           </div>
+          <div className=" grid grid-cols-2 gap-4 lg:pl-20">
+            {/* SiteMap */}
+            <div className="">
+              <div>
+                <p className="mb-2">Sitemap</p>
+                <ul>
+                  {sitemap.map(({ label, href }, key) => (
+                    <li key={key}>
+                      <a
+                        href={href}
+                        className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                      >
+                        {label}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              {/* Social */}
+              <div>
+                <p className="mb-2">Social</p>
+                <ul>
+                  {socials.map(({ label, href }, key) => (
+                    <li key={key}>
+                      <a
+                        href={href}
+                        target="_blank"
+                        className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                      >
+                        {label}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-between pt-10 mb-8  ">
+          <a href="" className="">
+            <img
+              src="/src/assets/images/logo.svg"
+              width={40}
+              height={40}
+              alt="Logo"
+            />
+          </a>
+          <p className="text-zinc-500 text-sm ">
+            &copy; 2025{" "}
+            <span className="text-zinc-200"> Manjur Al Jubayer</span>
+          </p>
         </div>
       </div>
     </footer>
