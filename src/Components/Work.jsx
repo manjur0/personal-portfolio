8792e294b2611854cac7cfc1419d93af7e075b85
manjur0 <sfmanjur21@gmail.com>
2025-02-28 15:06:10 +0600

    work image updated

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -3,42 +3,47 @@
  * License: Apache-2.0
  * Email: sfmanjur21@gmail.com
  */
+import project1 from "../assets/images/project-1.jpg";
+import project2 from "../assets/images/project-2.jpg";
+import project3 from "../assets/images/project-3.jpg";
+import project4 from "../assets/images/project-4.jpg";
+import project5 from "../assets/images/project-5.jpg";
+import project6 from "../assets/images/project-6.jpg";
 
 import ProjectCard from "./ProjectCard";
-
 const works = [
   {
-    imgSrc: "src/assets/images/project-1.jpg",
+    imgSrc: project1,
     title: "Full stack music app",
     tags: ["API", "MVC", "Development"],
     projectLink: "https://musify-5al0.onrender.com/",
   },
   {
-    imgSrc: "src/assets/images/project-2.jpg",
+    imgSrc: project2,
     title: "Free stock photo app",
     tags: ["API", "SPA"],
     projectLink: "https://pixstock-official.vercel.app/",
   },
   {
-    imgSrc: "src/assets/images/project-3.jpg",
+    imgSrc: project3,
     title: "Recipe app",
     tags: ["Development", "API"],
     projectLink: "",
   },
   {
-    imgSrc: "src/assets/images/project-4.jpg",
+    imgSrc: project4,
     title: "Real state website",
     tags: ["Web-design", "Development"],
     projectLink: "https://github.com/codewithsadee-org/wealthome",
   },
   {
-    imgSrc: "src/assets/images/project-5.jpg",
+    imgSrc: project5,
     title: "eCommerce website",
     tags: ["eCommerce", "Development"],
     projectLink: "https://github.com/codewithsadee/anon-ecommerce-website",
   },
   {
-    imgSrc: "src/assets/images/project-6.jpg",
+    imgSrc: project6,
     title: "vCard Personal portfolio",
     tags: ["Web-design", "Development"],
     projectLink: "https://github.com/codewithsadee/vcard-personal-portfolio",
