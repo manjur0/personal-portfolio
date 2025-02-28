7d047f77735fb273ecc0e7cb4222049f9020dd7e
manjur0 <sfmanjur21@gmail.com>
2025-02-28 15:20:47 +0600

    Update about article

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -23,11 +23,11 @@ const About = () => {
       <div className="container">
         <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up ">
           <p className="text-zinc-300 mb-4 md:mb-8 md:text-lg md:max-w[60ch] ">
-            Welcome! I&apos;m Henry, a professional web developer with a knack
-            for crafting visually stunning and highly functional websites.
-            Combining creativity and technical expertise. I transform your
-            vision into digital masterpiece that excels in both appearance and
-            performance.
+            Welcome! I'm Manjur, a passionate web developer specializing in
+            crafting visually stunning and highly functional websites. With a
+            keen eye for design and a strong technical foundation, I bring ideas
+            to life—transforming visions into seamless digital experiences that
+            are both aesthetically appealing and performance-driven.
           </p>
           <div className="flex flex-wrap items-center gap-4 md:gap-7">
             {aboutItems.map(({ label, number }, key) => (
